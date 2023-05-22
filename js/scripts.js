e6bdce960c19d06608b0b23207d586f5073d42b9
eyos <eyosiastamirat20@gmail.com>
2023-05-22 20:24:28 +0300

    ...

--- js/scripts.js
+++ js/scripts.js
@@ -9,9 +9,9 @@ window.onload = function () {
     var hours = date.getHours();
     var minutes = date.getMinutes();
     var current = hours + minutes * 0.01;
-    if (current >= 5 && current < 19) return "Have a nice day";
-    if (current >= 19 && current < 22) return "Have a nice evening";
-    if (current >= 22 || current < 5) return "Have a good night";
+    if (current >= 5 && current < 19) return "Have a nice day 🌞";
+    if (current >= 19 && current < 22) return "Have a nice evening 🌙";
+    if (current >= 22 || current < 5) return "Have a good night 🛏";
   };
 
   var messages = [
